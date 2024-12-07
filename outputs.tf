b733e41085c9df21c32024c2aba45466cd3115da
chut-th <chutchawin.th@outlook.com>
2024-12-07 12:43:51 +0700

    Add resource group location

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "resource_group_id" {
   description = "The ID of the resource group"
   value       = azurerm_resource_group.rg.id
 }
+
+output "resource_group_location" {
+  description = "The location of the resource group"
+  value       = azurerm_resource_group.rg.location
+}
